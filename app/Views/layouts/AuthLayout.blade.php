7bbad2d36920468ed49790a6df61f742d7cb03ec
stefanuspet <stefanuspetra.p@gmail.com>
2024-10-02 07:35:46 +0700

    login Bg

--- app/Views/layouts/AuthLayout.blade.php
+++ app/Views/layouts/AuthLayout.blade.php
@@ -22,13 +22,19 @@
 </head>
 
 <body>
-    <div class="w-full h-dvh bg-slate-100 ">
-        <div class="w-full h-full flex justify-center items-center">
+    <div class="w-full h-dvh" style="background-image: url('/assets/bg_auth.png'); background-size: cover; background-position: center;">
+        <div class="w-full h-full grid grid-rows-3 place-items-center bg-[#2A508A] bg-opacity-70">
+            <div class="flex items-center gap-x-10">
+                <img src="/assets/logo_1.png" alt="logo_bandung_karate_club">
+                <h1 class="font-bold text-white text-2xl text-center">Bandung <br> Karate Club</h1>
+                <img src="/assets/logo_2.png" alt="logo_forki">
+            </div>
             <div class="bg-white w-[24rem] py-6 px-8 rounded-md shadow-lg">
                 @yield('content')
             </div>
         </div>
     </div>
 </body>
+
 
 </html>
